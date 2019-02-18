eeaac0f90cddf835da0b8792a82ff96d5ace2ce9
QAQorz <1059037957@qq.com>
2019-02-18 14:41:17 +0800

    jls线段树，维护区间最大值，最大值数，次大值，sum + 快读

---
+++ HDU_5306.cpp
@@ -0,0 +1,119 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#define INF 0x3f3f3f3f
+#define LL long long
+#define debug(x) cout << "[" << x << "]" << endl
+using namespace std;
+#define lid id << 1
+#define rid id << 1 | 1
+
+const int N = 1e6+7;
+int a[N];
+struct tree{
+	int l, r, num;
+	LL sum, mx, smx;
+}tree[N<<2];
+
+void push_up(int id){
+	tree[id].sum = tree[lid].sum + tree[rid].sum;
+	if (tree[lid].mx == tree[rid].mx){
+		tree[id].mx = tree[lid].mx;
+		tree[id].num = tree[lid].num + tree[rid].num;
+		tree[id].smx = max(tree[lid].smx, tree[rid].smx);
+	}
+	else {
+		tree[id].mx = max(tree[lid].mx, tree[rid].mx);
+		tree[id].num = (tree[lid].mx > tree[rid].mx) ? tree[lid].num : tree[rid].num;
+		tree[id].smx = max(min(tree[lid].mx, tree[rid].mx), max(tree[lid].smx, tree[rid].smx));
+	}
+}
+
+void build(int l, int r, int id){
+	tree[id].l = l;
+	tree[id].r = r;
+	if (l == r){
+		tree[id].mx = tree[id].sum = a[l];
+		tree[id].smx = -1;
+		tree[id].num = 1;
+		return;
+	}
+	int mid = (l+r)>>1;
+	build(l, mid, lid);
+	build(mid+1, r, rid);
+	push_up(id);
+}
+
+void push_down(int id, int x){
+	if (x >= tree[id].mx) return;
+	tree[id].sum -= tree[id].num * (tree[id].mx-x);
+	tree[id].mx = x;
+}
+
+void upd(int l, int r, int id, int x){
+	if (tree[id].mx <= x) return;
+	if (tree[id].l >= l && tree[id].r <= r && tree[id].smx < x){
+		push_down(id, x);
+		return;
+	}
+	push_down(lid, tree[id].mx);
+	push_down(rid, tree[id].mx);
+	int mid = (tree[id].l + tree[id].r)>>1;
+	if (r <= mid) upd(l, r, lid, x);
+	else if (mid < l) upd(l, r, rid, x);
+	else upd(l, mid, lid, x), upd(mid+1, r, rid, x);
+	push_up(id);
+}
+
+LL query(int l, int r, int id, bool f){
+	if (tree[id].l == l && tree[id].r == r){
+		if (f) return tree[id].sum;
+		return tree[id].mx;
+	}
+	push_down(lid, tree[id].mx);
+	push_down(rid, tree[id].mx);
+	int mid = (tree[id].l + tree[id].r)>>1;
+	if (r <= mid) return query(l, r, lid, f);
+	if (mid < l) return query(l, r, rid, f);
+	return f ? query(l, mid, lid, f) + query(mid+1, r, rid, f) : max(query(l, mid, lid, f), query(mid+1, r, rid, f));
+}
+
+template <class T>
+inline bool scan_d(T &ret) {
+    char c;
+    int sgn;
+    if (c = getchar(), c == EOF) return 0;
+    while (c != '-' && (c < '0' || c > '9')) c = getchar();
+    sgn = (c == '-' ? -1 : 1);
+    ret = (c == '-') ? 0 : (c - '0');
+    while (c = getchar(), c >= '0' && c <= '9') ret = ret * 10 + (c - '0');
+    ret *= sgn;
+    return 1;
+}
+
+int main(){
+	freopen("/home/qaqorz/PikaGu/test.txt", "r", stdin);
+	int t, n, q;
+	scan_d<int>(t);
+	while (t--){
+		scan_d<int>(n);
+		scan_d<int>(q);
+		for (int i = 1; i <= n; i++) scan_d<int>(a[i]);
+		build(1, n, 1);
+		int op, l, r, x;
+		while (q--){
+			scan_d<int>(op);
+			scan_d<int>(l);
+			scan_d<int>(r);
+			if (!op){
+				scan_d<int>(x);
+				upd(l, r, 1, x);
+			}
+			else if (op == 1) printf("%lld\n", query(l, r, 1, 0));
+			else printf("%lld\n", query(l, r, 1, 1));
+		}
+	}
+	return 0;
+}
